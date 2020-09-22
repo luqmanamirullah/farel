c7900d22bbff6faf28d449b59c0d238c5385582d
Abui <adjiem31@gmail.com>
2020-09-22 12:38:50 +0700

    Add : Theme and React-JSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
-
+import { useEffect } from "react";
+import { ThemeProvider } from "react-jss";
+import Theme from "theme/Theme";
+import "~/styles/globals.css";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const style = document.getElementById("server-side-styles");
+
+    if (style) {
+      style.parentNode.removeChild(style);
+    }
+  });
+  return (
+    <>
+      {/* <div id="wrapper" class="clearfix"> */}
+      <Theme>
+        <Component {...pageProps} />
+      </Theme>
+
+      {/* </div> */}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
