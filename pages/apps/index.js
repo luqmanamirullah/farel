beb99c3c7d15f5f067a70b28208225bef12faf16
tari budi <lestaritari113@gmail.com>
2021-02-10 14:21:58 +0700

    image seo
    baru halaman index sama apps

--- pages/apps/index.js
+++ pages/apps/index.js
@@ -18,6 +18,56 @@ const SvarOnlineIndex = () => {
 						locale: 'en_IE',
 						url: 'https://svara.id/',
 						site_name: 'Svara Apps',
+						images: [
+							{
+								url: 'https://svara.id/assets_bg-apps.png',
+								width: 1185,
+								height: 667,
+								alt: 'Svara Apps'
+							},
+							{
+								url: 'https://svara.id/assets_mobile.png',
+								width: 652,
+								height: 426,
+								alt: 'Svara Innovation Svara Apps Mobile'
+							},
+							{
+								url: 'https://svara.id/assets_tab.png',
+								width: 142,
+								height: 142,
+								alt: 'Svara Innovation Svara Apps Tab'
+							},
+							{
+								url: 'https://svara.id/assets_mac.png',
+								width: 142,
+								height: 142,
+								alt: 'Svara Innovation Svara Apps Webplay'
+							},
+							{
+								url: 'https://svara.id/fitur.png',
+								width: 142,
+								height: 142,
+								alt: 'Svara Apps, Webplay Fitur'
+							},
+							{
+								url: 'https://svara.id/apps/connectedCar.JPG',
+								width: 360,
+								height: 210,
+								alt: 'Svara Apps Connected Car'
+							},
+							{
+								url: 'https://svara.id/apps/smartSpeaker.JPG',
+								width: 360,
+								height: 210,
+								alt: 'Svara Apps Smart Speaker'
+							},
+							{
+								url: 'https://svara.id/apps/smartTV.JPG',
+								width: 360,
+								height: 210,
+								alt: 'Svara Apps Smart TV'
+							}
+						],
 						description:
 							'SVARA is a broadcasting platform to help Industry Radio in doing Digital Transformation and also for listeners to enjoy a new way of listening to radio, music and podcasts. There are several main features offered, including; Modernization on-air platform, monetize on-line platform.'
 					}}
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function HomePage() {
 			<Head>
 				<title>Svara | The Sound of You</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link rel="shortcut icon" type="image/png" sizes="32x32" href="/public/logo/logo-gram only/apps.png" />
 			</Head>
 			<BasicLayout>
 				<DefaultSeo
@@ -19,6 +20,110 @@ export default function HomePage() {
 						locale: 'en_IE',
 						url: 'https://svara.id/',
 						site_name: 'SVARA',
+						images: [
+							{
+								url: 'https://svara.id/Group%201.png',
+								width: 599,
+								height: 392,
+								alt: 'Svara Product'
+							},
+							{
+								url: 'https://svara.id/logo/logo.png',
+								width: 142,
+								height: 142,
+								alt: 'Svara Innovation Svara Apps'
+							},
+							{
+								url: 'https://svara.id/logo/SVARA%20B2B%20LOGO-COLORED-03.png',
+								width: 142,
+								height: 142,
+								alt: 'Svara Innovation Svara Aircast'
+							},
+							{
+								url: 'https://svara.id/logo/SVARA%20B2B%20LOGO-COLORED-02.png',
+								width: 142,
+								height: 142,
+								alt: 'Svara Innovation Svara Soundsight'
+							},
+							{
+								url: 'https://svara.id/logo/SVARA%20B2B%20LOGO-COLORED-01.png',
+								width: 142,
+								height: 142,
+								alt: 'Svara Innovation Svara Castline'
+							},
+							{
+								url: 'https://svara.id/logo/SVARA%20B2B%20LOGO-COLORED-04.png',
+								width: 142,
+								height: 142,
+								alt: 'Svara Innovation Svara Ads'
+							},
+							{
+								url: 'https://svara.id/partner/1.jpg',
+								width: 400,
+								height: 300,
+								alt: 'UMG Idea Lab Partner SVARA'
+							},
+							{
+								url: 'https://svara.id/partner/2.jpg',
+								width: 400,
+								height: 300,
+								alt: 'Widya Wicara Partner SVARA'
+							},
+							{
+								url: 'https://svara.id/partner/3.jpg',
+								width: 400,
+								height: 300,
+								alt: 'WAMI (Wahana Musik Indonesia) Partner SVARA'
+							},
+							{
+								url: 'https://svara.id/partner/4.jpg',
+								width: 400,
+								height: 300,
+								alt: 'IDX Incubator Partner SVARA'
+							},
+							{
+								url: 'https://svara.id/partner/5.jpg',
+								width: 400,
+								height: 300,
+								alt: 'bahasa kita Partner SVARA'
+							},
+							{
+								url: 'https://svara.id/partner/6.jpg',
+								width: 400,
+								height: 300,
+								alt: 'triton Partner SVARA'
+							},
+							{
+								url: 'https://svara.id/partner/7.jpg',
+								width: 400,
+								height: 300,
+								alt: 'telkomsel Partner SVARA'
+							},
+							{
+								url: 'https://svara.id/partner/8.jpg',
+								width: 400,
+								height: 300,
+								alt: 'LPiK ITB Partner SVARA'
+							},
+							{
+								url: 'https://svara.id/partner/9.jpg',
+								width: 400,
+								height: 300,
+								alt: 'Ristekdikti Partner SVARA'
+							},
+							{
+								url: 'https://svara.id/partner/10.jpg',
+								width: 400,
+								height: 300,
+								alt: 'Institu Teknologi Bandung (ITB) Partner SVARA'
+							},
+							{
+								url: 'https://svara.id/partner/11.jpg',
+								width: 400,
+								height: 300,
+								alt: 'Solusi 247 Partner SVARA'
+							}
+						],
 						description: 'radio music podcast'
 					}}
 					twitter={{
