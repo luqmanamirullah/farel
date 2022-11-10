bdea4f1395c7fe5e2279b8d9a1cc6a6a6732f3ab
ghalib77 <muhamadghalib7@gmail.com>
2022-11-10 18:03:08 +0700

    style: remove inline-style and improve partner responsive

--- components/airtime/airtime.js
+++ components/airtime/airtime.js
@@ -1,84 +1,72 @@
-import React from 'react';
-import { createUseStyles } from 'react-jss';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import CardTitle from '../card/ImageRight';
-import CardTitleLeft from '../card/ImageLeft';
-import Description from '../card/ImageRight/descriptionLeft';
-import ListFitur from '../List/ListRounded';
-import ClearFix from '../container/ClearFix';
-import detail from '../../contents/airtime.json';
-const useStyles = createUseStyles({});
+import React from "react";
+import { createUseStyles } from "react-jss";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CardTitle from "../card/ImageRight";
+import CardTitleLeft from "../card/ImageLeft";
+import Description from "../card/ImageRight/descriptionLeft";
+import ListFitur from "../List/ListRounded";
+import ClearFix from "../container/ClearFix";
+import detail from "../../contents/airtime.json";
+import { useMediaQuery } from "utils/hooks/useMedia";
+
+const useStyles = createUseStyles({
+  title: {
+    color: "#91b3c2",
+    fontSize: 30,
+    fontWeight: 700,
+    fontFamily: "Nunito",
+    textAlign: "center",
+    margin: "50px 0px 30px 0px",
+  },
+  desc: {
+    backgroundColor: "#91b3c2",
+    padding: "60px 90px 60px 90px",
+    fontWeight: 550,
+    color: "white",
+    fontSize: "clamp(1.3rem, 2vw, 1.875rem)",
+    textAlign: "center",
+    lineHeight: 2,
+  },
+});
 
 export default function CenteredGrid() {
-	const classes = useStyles();
+  const classes = useStyles();
+  const matches900 = useMediaQuery("(min-width:960px)");
 
-	return (
-		<div style={{ fontFamily: 'Nunito' }}>
-			<CardTitle
-				img={detail.section1.image}
-				title={detail.section1.title}
-				desc={detail.section1.desc}
-				colorFont={detail.section1.colorFont}
-				height="460px"
-				marginTop="95px"
-			/>
-			<CardTitleLeft
-				headline={detail.section2.headline}
-				description={detail.section2.description}
-				colorFont={detail.section2.colorFont}
-				backgroundColor={detail.section2.backgroundColor}
-				img={detail.section2.image}
-			/>
-			<div
-				style={{
-					color: '#91b3c2',
-					fontSize: 30,
-					fontWeight: 700,
-					fontFamily: 'Nunito',
-					textAlign: 'center',
-					margin: '50px 0px 30px 0px'
-				}}
-			>
-				{detail.section3.title}
-			</div>
-			<ListFitur data={detail.section3.benefit} />
-			<div
-				style={{
-					backgroundColor: '#91b3c2',
-					padding: '60px 90px 60px 90px',
-					fontWeight: 550,
-					color: 'white',
-					fontSize: 30,
-					textAlign: 'center',
-					lineHeight: 2
-				}}
-			>
-				{detail.section4.desc}
-			</div>
-			<div
-				style={{
-					color: '#91b3c2',
-					fontSize: 30,
-					fontWeight: 700,
-					fontFamily: 'Nunito',
-					textAlign: 'center',
-					margin: '50px 0px 50px 0px'
-				}}
-			>
-				KLIEN KAMI
-			</div>
-			<ClearFix style={{ marginBottom: 100 }}>
-				<Row style={{ margin: 0 }} className="justify-content-md-center">
-					{detail.section5.map((partner) => {
-						return (
-							<Col lg={2} md="auto" xs={12}>
-								<img className={classes.img} src={partner.image} />
-							</Col>
-						);
-					})}
-				</Row>
-			</ClearFix>
-		</div>
-	);
+  return (
+    <div style={{ fontFamily: "Nunito" }}>
+      <CardTitle
+        img={detail.section1.image}
+        title={detail.section1.title}
+        desc={detail.section1.desc}
+        colorFont={detail.section1.colorFont}
+        marginTop={matches900 && "90px"}
+      />
+      <CardTitleLeft
+        headline={detail.section2.headline}
+        description={detail.section2.description}
+        colorFont={detail.section2.colorFont}
+        backgroundColor={detail.section2.backgroundColor}
+        img={detail.section2.image}
+      />
+      <div>
+        <div className={classes.title}>{detail.section3.title}</div>
+        <ListFitur data={detail.section3.benefit} />
+        <div className={classes.desc}>{detail.section4.desc}</div>
+        <div className={classes.title}>KLIEN KAMI</div>
+      </div>
+      <ClearFix style={{ marginBottom: 100 }}>
+        <Row style={{ margin: 0 }} className="justify-content-md-center">
+          {detail.section5.map((partner) => {
+            return (
+              <Col xs={6} md={6} lg={2}>
+                <img className={classes.img} src={partner.image} />
+              </Col>
+            );
+          })}
+        </Row>
+      </ClearFix>
+    </div>
+  );
 }
